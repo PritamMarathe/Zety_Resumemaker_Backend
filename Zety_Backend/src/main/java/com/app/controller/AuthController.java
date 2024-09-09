a5263c4b7d36013d4519de954419c6e5fad49a59
PritamMarathe <pritam123marathe@gmail.com>
2024-09-09 13:40:13 +0530

    Facebook Deletion URL Solution

--- Zety_Backend/src/main/java/com/app/controller/AuthController.java
+++ Zety_Backend/src/main/java/com/app/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.app.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +11,12 @@ public class AuthController {
 	    public String facebookLogin() {
 	        // Redirect to the Facebook login URL
 	        return "redirect:/oauth2/authorization/facebook";
+	    }
+	 
+	 @GetMapping("/data-deletion")
+	    public ResponseEntity<String> getDataDeletionInstructions() {
+	        String instructions = "To request data deletion, please email us at support@yourapp.com with your username or registered email.";
+	        return new ResponseEntity<>(instructions, HttpStatus.OK);
 	    }
 	 
 	 @GetMapping("/auth/login/google")
